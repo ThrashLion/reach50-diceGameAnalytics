4be868fda3a928363de6647f09cff7e27e77b5c2
ThrashLion <49458215+ThrashLion@users.noreply.github.com>
2022-04-08 22:46:29 +0200

    EMP sigma->CalculatorService refactoring

--- src/contantThreshold/EmpiricalAnalysis.kt
+++ src/contantThreshold/EmpiricalAnalysis.kt
@@ -1,8 +1,8 @@
 package contantThreshold
 
 import GameRuleset
+import Service.CalculatorService
 import ValueSigma
-import kotlin.math.pow
 
 class EmpiricalAnalysis (val ruleset: GameRuleset, var sampleSize: Int) {
 
@@ -24,16 +24,9 @@ class EmpiricalAnalysis (val ruleset: GameRuleset, var sampleSize: Int) {
             sumOfAllRounds += rounds
         }
         val average = sumOfAllRounds.toDouble() / sampleSize.toDouble()
-        val sigma = calculateSigma(resultList,average)
+        val sigma = CalculatorService.calculateSigma(resultList,average,sampleSize)
         val resultSigma = ValueSigma(average,sigma)
         println("$threshold $resultSigma")
     }
 
-    private fun calculateSigma(resultList: MutableList<Int>, average: Double): Double {
-        var sumOfSquares: Double = 0.0
-        for (i in 0 until sampleSize) {
-            sumOfSquares += (resultList[i]-average).pow(2)
-        }
-        return (1/(sampleSize.toDouble()-1)).pow(0.5)
-    }
 }
